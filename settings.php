b7653702e603b859a25b109ee750e6beaccf147a
Solvanar <146109761+Solvanar@users.noreply.github.com>
2026-04-16 12:51:22 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-define('C_REST_CLIENT_ID','local.69e0aa680f4cc5.24139748');//Application ID
-define('C_REST_CLIENT_SECRET','AjoQjO6Z46rBaYzQPQ7b1zOYpXnbf4IqHyExpfPwiGJDi8RelV');//Application key
+define('C_REST_CLIENT_ID','local.69e0b0f659a758.41700726');//Application ID
+define('C_REST_CLIENT_SECRET','NBbvoTvLpfPZoGzRSJJxw6wqnDPQnF0Hl3mdDrTczWpEQbHf8x');//Application key
 // or
 //define('C_REST_WEB_HOOK_URL','https://rest-api.bitrix24.com/rest/1/doutwqkjxgc3mgc1/');//url on creat Webhook
 
